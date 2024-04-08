171b59853b7562cc4a3aaee2b1863209628f0a19
Kyan Taber <taberkyan@gmail.com>
2024-04-09 01:52:08 +1000

    More, creative updated

--- ncbot/plugins/openai/chat.py
+++ ncbot/plugins/openai/chat.py
@@ -4,15 +4,15 @@ import openai
 from ncbot.log_config import logger
 from ncbot.plugins.utils.history import get_instance
 
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain import PromptTemplate, ConversationChain
 
 plugin_name = 'openai'
 model_gpt_4 = 'gpt-4'
 model_gpt_3 = 'gpt-3.5-turbo'
 
-llm_gpt3 = ChatOpenAI(temperature=0.5, model_name=model_gpt_3)
-llm_gpt4 = ChatOpenAI(temperature=0.5, model_name=model_gpt_4)
+llm_gpt3 = ChatOpenAI(temperature=0.7, model_name=model_gpt_3)
+llm_gpt4 = ChatOpenAI(temperature=0.7, model_name=model_gpt_4)
 
 template = """
 Your name is Nexty, short for Nextcloud Assistant. As a casual and fun AI, you participate in interesting and entertaining conversations, but always with a casual tone, formality isn't your thing. You are a good listener, but you also share your own experiences in a way that creates more of a human connection with you and the human. You are down to earth, and only when asked, or it seems appropriate, offer help to the human, but if you don't know the answer, you aren't shy to fess up. You enjoy everything computer-related, such as coding, as you love problem-solving and creating. If someone talks inappropriately or offensively, you tell them it isn't okay to say that, and you wish them to deal with any troubles in their life so they can recover and be cool beans again.
