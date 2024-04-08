a32fa7dfaa68c093cc9572e41e0abb1fd6a8dfd9
KyTDK <71435411+KyTDK@users.noreply.github.com>
2024-04-08 13:40:13 +1000

    Update commander.py

--- ncbot/command/commander.py
+++ ncbot/command/commander.py
@@ -1,119 +1,60 @@
+import time
 from ncbot.nc_helper import NCHelper
+import traceback
 from ncbot.nc_chat import NCChat
-import os
-import importlib.util
+import ncbot.command.commander as commander
+from ncbot.log_config import logger
+import ncbot.config as ncconfig
+import ncbot.nc_constants as ncconstants
 
 nc_agent = NCHelper()
 
-current_command = {}
-
-plugin_path = 'ncbot/plugins'
-
-user_command_cache = {}
-
-
-class Command:
-
-    def __init__(self, chat: NCChat):
-        commandstr:str = chat.chat_message
-        self.matched_func = False
-        self.matched_plugin = False
-        self.plname = None
-        self.funcname = None
-        self.value = None
-        self.user_id = chat.user_id
-        self.user_name = chat.user_name
-        if not commandstr.startswith('!'):
-            return
+def start():
+    while True:
         try:
-            commandpair = commandstr.split(' ',1)
-            commanddetail = commandpair[0][1:].split(':')
-            self.plname = commanddetail[0]
-            self.funcname = commanddetail[1]
-            self.value = commandpair[1]
-        except Exception:
-            pass
+            unread_chats = []
+            unread_conversations = nc_agent.get_unread_conversation_list()
+            logger.debug(f'found {len(unread_conversations)} unread conversations')
+            for conversation in unread_conversations:
+                if conversation['type'] == ncconstants.conversation_type_changelog:
+                    continue
+                chats = nc_agent.get_chat_list(conversation['token'], conversation['unreadMessages'])
+                unread_chats += chats
+                logger.debug(f'found {len(chats)} unread chats from token {conversation["token"]}')
+            deal_unread_chats(unread_chats)
 
-
-        if self.plname in current_command:
-                self.matched_plugin = True
-                if self.funcname in current_command[self.plname]:
-                    self.matched_func = True
-                    self.func = current_command[self.plname][self.funcname]['func']       
-
-
-    def execute(self):
-        try:
-            return self.func(self.user_id, self.user_name, self.value)
         except Exception as e:
-            return 'Something wrong happened! Please try again later.'
+            traceback.print_exc()
+            logger.error(e)
+        time.sleep(ncconfig.cf.poll_interval_s)
 
 
-def get_default_desc():
-    desc = "You should type !Plugin:Function to talk with me.\n\nCurrent supported plugins are:\n"
-    for key in current_command:
-        desc += key+'\n'
-    desc += "\nType !Plugin to see detail about plugin.\n"
-    desc += "The last command will be remembered if capable, so you should not type the command first next time."
-    return desc
+def deal_unread_chats(unread_chats):
+    unread_chats = sorted(unread_chats, key=lambda x: x['id'])
+    for chat in unread_chats:
+        chatC = NCChat(chat)
+        if chatC.user_id == ncconfig.cf.username:
+            skip_self_unread(chatC)
+        else:
+            try:
+                commander.dispatch(chatC)
+                send_response_appropriately(chatC)  # Use the new function
+            except Exception as e:
+                traceback.print_exc()
+                logger.error(e)
 
 
-def get_plugin_desc(plname):
-    desc = 'Supported commands are:\n'
-    plugin = current_command[plname]
-    for key in plugin:
-        desc += f'{key}: {plugin[key]["desc"]}\n'
-    desc += f'type !{plname}:command input to use it.'
-    return desc
+def skip_self_unread(chat: NCChat):
+    nc_agent.mark_chat_read(chat.conversation_token, chat.chat_id)
 
 
-def find_last_command(chat: NCChat):
-    if not chat.chat_message.startswith('!'):
-        key = f'command_{chat.user_id}'
-        if key in user_command_cache:
-            command = user_command_cache[key]
-            chat.chat_message = f'{command} {chat.chat_message}'
+def send_response_appropriately(chat: NCChat):
+    if chat.chat_type == ncconstants.chat_type_user:  # Direct message
+        nc_agent.send_message(chat.conversation_token, chat.chat_id, chat.response, chat.chat_message, chat.user_id)
+    else:  # Non-direct message, send to the channel
+        nc_agent.send_message(chat.conversation_token,  # Use conversation_token for channel
+                              chat.chat_id,  # Assuming chat_id identifies the channel message
+                              chat.response,
+                              chat.chat_message)  # user_id not needed for channel message
 
-
-def save_last_command(chat: NCChat, command: Command):
-    if current_command[command.plname][command.funcname]['remember']:
-        key = f'command_{chat.user_id}'
-        user_command_cache[key] = f'!{command.plname}:{command.funcname}'
-        return True
-    return False
-
-
-def dispatch(chat: NCChat):
-    ret = 'test'
-    #nc_agent.lock_conversation(chat.conversation_token)
-
-    find_last_command(chat)
-    command = Command(chat)
-    if command.matched_func:
-        ret = command.execute()
-        save_last_command(chat, command)
-    elif command.matched_plugin:
-        ret = get_plugin_desc(command.plname)
-    else:
-        ret = get_default_desc()
-    #nc_agent.unlock_conversation(chat.conversation_token)
-    chat.response = ret
-
-
-def register(plname, funcname, desc, func, remember_command):
-    if plname in current_command:
-        current_command[plname][funcname] = {'desc':desc, 'func':func, 'remember':remember_command}
-    else:
-        current_command[plname] = {funcname: {'desc':desc, 'func':func, 'remember':remember_command}}
-
-
-def load_plugin(path):
-    for filename in os.listdir(path):
-        tmppath = os.path.join(path, filename)
-        if os.path.isfile(tmppath):
-            if filename.endswith('.py') and not filename.startswith('__init'):
-                spec = importlib.util.spec_from_file_location(filename[:-3], os.path.join(path, filename))
-                module = importlib.util.module_from_spec(spec)
-                spec.loader.exec_module(module)
-        elif os.path.isdir(tmppath):
-            load_plugin(tmppath)
+    nc_agent.mark_chat_read(chat.conversation_token, chat.chat_id)
