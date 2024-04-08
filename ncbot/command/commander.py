530d083aeeac099c082ddfd9793a12916ec7009b
KyTDK <71435411+KyTDK@users.noreply.github.com>
2024-04-08 14:33:37 +1000

    Update commander.py

--- ncbot/command/commander.py
+++ ncbot/command/commander.py
@@ -1,5 +1,6 @@
 from ncbot.nc_helper import NCHelper
 from ncbot.nc_chat import NCChat
+from ncbot.plugins.openai.chat as openai
 import os
 import importlib.util
 
@@ -10,44 +11,6 @@ current_command = {}
 plugin_path = 'ncbot/plugins'
 
 user_command_cache = {}
-
-
-class Command:
-
-    def __init__(self, chat: NCChat):
-        commandstr:str = chat.chat_message
-        self.matched_func = True
-        self.matched_plugin = True
-        self.plname = "openai"
-        self.funcname = "chat3"
-        self.value = None
-        self.user_id = chat.user_id
-        self.user_name = chat.user_name
-        if not commandstr.startswith('!'):
-            return
-        try:
-            commandpair = commandstr.split(' ',1)
-            commanddetail = commandpair[0][1:].split(':')
-            self.plname = commanddetail[0]
-            self.funcname = commanddetail[1]
-            self.value = commandpair[1]
-        except Exception:
-            pass
-
-
-        if self.plname in current_command:
-                self.matched_plugin = True
-                if self.funcname in current_command[self.plname]:
-                    self.matched_func = True
-                    self.func = current_command[self.plname][self.funcname]['func']       
-
-
-    def execute(self):
-        try:
-            return self.func(self.user_id, self.user_name, self.value)
-        except Exception as e:
-            return 'Something wrong happened! Please try again later.'
-
 
 def get_default_desc():
     desc = "You should type !Plugin:Function to talk with me.\n\nCurrent supported plugins are:\n"
@@ -66,31 +29,11 @@ def get_plugin_desc(plname):
     desc += f'type !{plname}:command input to use it.'
     return desc
 
-
-def find_last_command(chat: NCChat):
-    if not chat.chat_message.startswith('!'):
-        key = f'command_{chat.user_id}'
-        if key in user_command_cache:
-            command = user_command_cache[key]
-            chat.chat_message = f'{command} {chat.chat_message}'
-
-
-def save_last_command(chat: NCChat, command: Command):
-    if current_command[command.plname][command.funcname]['remember']:
-        key = f'command_{chat.user_id}'
-        user_command_cache[key] = f'!{command.plname}:{command.funcname}'
-        return True
-    return False
-
-
 def dispatch(chat: NCChat):
     ret = 'test'
     #nc_agent.lock_conversation(chat.conversation_token)
-
-    find_last_command(chat)
-    command = Command(chat)
     if command.matched_func:
-        ret = command.execute()
+        ret = openai.chat3(chat.user_id, chat.user_name, chat.chat_message)
         save_last_command(chat, command)
     elif command.matched_plugin:
         ret = get_plugin_desc(command.plname)
