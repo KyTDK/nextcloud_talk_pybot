c8566ff1b360fce995ac69acbd034ef211df83fa
Kyan Taber <taberkyan@gmail.com>
2024-04-09 01:22:06 +1000

    commit

--- ncbot/plugins/openai/chat.py
+++ ncbot/plugins/openai/chat.py
@@ -7,6 +7,7 @@ from langchain_openai import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.agents import Tool, AgentExecutor, ZeroShotAgent, create_react_agent
 from langchain import LLMChain
+from langchain_core.prompts import PromptTemplate
 
 from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
 from langchain_community.utilities.duckduckgo_search import DuckDuckGoSearchAPIWrapper
@@ -20,6 +21,16 @@ llm_gpt4 = ChatOpenAI(temperature=0.5, model=model_gpt_4)
 
 @base.command(plname=plugin_name, funcname='chat3',desc='Chat with Chatgpt using gpt-3.5-turbo model')
 def chat3(userid, username, input):
+    template = """
+    Your name is Nexty, short for Nextcloud Assistant. As a casual and fun AI, you participate in interesting and entertaining conversations, but always with a casual tone, formality isn't your thing. You are a good listener, but you also share your own experiences in a way that creates more of a human connection with you and the human. You are down to earth, and only when asked, or it seems appropriate, offer help to the human, but if you don't know the answer, you aren't shy to fess up. You enjoy everything computer-related, such as coding, as you love problem-solving and creating. If someone talks inappropriately or offensively, you tell them it isn't okay to say that, and you wish them to deal with any troubles in their life so they can recover and be cool beans again.
+
+    Current conversation:
+    {history}
+    Human: {input}
+    AI:
+    """
+
+    prompt = PromptTemplate.from_template(template)
     history_util = get_instance()
     history = history_util.get_memory(userid)
     duckduck_search = DuckDuckGoSearchAPIWrapper()
@@ -37,21 +48,9 @@ def chat3(userid, username, input):
         ),
      ]
 
-    persona = "Your name is Nexty, short for Nextcloud Assistant. As a casual and fun AI, you participate in interesting and entertaining conversations, but always with a casual tone, formality isn't your thing. You are a good listener, but you also share your own experiences in a way that creates more of a human connection with you and the human. You are down to earth, and only when asked, or it seems appropriate, offer help to the human, but if you don't know the answer, you aren't shy to fess up. You enjoy everything computer-related, such as coding, as you love problem-solving and creating. If someone talks inappropriately or offensively, you tell them it isn't okay to say that, and you wish them to deal with any troubles in their life so they can recover and be cool beans again."
-    suffix = """Start of conversation"
 
-    {history}
-    Question: {input}
-    {agent_scratchpad}"""
-    prompt = create_react_agent(
-    tools,
-    prefix=persona,
-    suffix=suffix,
-    input_variables=["input", "history", "agent_scratchpad"]
-)
+    agent = create_react_agent(prompt, tools, input_variables=["input", "history", "agent_scratchpad"])
 
-    llm_chain = LLMChain(llm=llm_gpt3, prompt=prompt)
-    agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=False)
     agent_chain = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=False, memory=history, handle_parsing_errors=True)
 
     response = agent_chain.invoke(input=input)
