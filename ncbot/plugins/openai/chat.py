26c4197cd9790b777ad4b1842b3f5002b75fa0a7
KyTDK <71435411+KyTDK@users.noreply.github.com>
2024-04-11 05:31:12 +1000

    Update chat.py

--- ncbot/plugins/openai/chat.py
+++ ncbot/plugins/openai/chat.py
@@ -43,7 +43,7 @@ def chat3(userid, username, input):
     agent = create_openai_tools_agent(llm, tools, prompt)
     # Create an agent executor by passing in the agent and tools
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-    response = agent_executor.invoke({"input": input, history.abuffer_as_messages()}, return_only_outputs=True)
+    response = agent_executor.invoke({"input": input, "history": history}, return_only_outputs=True)
     history_util.save_memory(userid, history)
     return response['output']
     
