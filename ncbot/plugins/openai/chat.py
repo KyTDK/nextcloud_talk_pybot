4e50bde2f23e19a60c806f533eec51a7a22f6365
KyTDK <71435411+KyTDK@users.noreply.github.com>
2024-04-12 01:18:17 +1000

    Update chat.py

--- ncbot/plugins/openai/chat.py
+++ ncbot/plugins/openai/chat.py
@@ -19,6 +19,8 @@ from langchain_community.tools.playwright.utils import (
 
 from datetime import datetime
 
+import nest_asyncio
+
 plugin_name = 'openai'
 model_gpt_4 = 'gpt-4'
 model_gpt_3 = 'gpt-3.5-turbo'
@@ -27,6 +29,7 @@ llm_gpt3 = ChatOpenAI(temperature=0.7, model_name=model_gpt_3)
 
 @base.command(plname=plugin_name, funcname='chat3',desc='Chat with Chatgpt using gpt-3.5-turbo model')
 async def chat3(userid, username, input):
+    nest_asyncio.apply()
     history_util = get_instance()
     history = history_util.get_memory(userid).load_memory_variables({})['history']
     llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
