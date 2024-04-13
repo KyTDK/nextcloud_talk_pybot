3ec0f47f8d33ee101656b86c4875d17639b7b6d7
Kyan Taber <taberkyan@gmail.com>
2024-04-13 18:54:45 +1000

    c

--- ncbot/plugins/utils/custom_agent_maker.py
+++ ncbot/plugins/utils/custom_agent_maker.py
@@ -33,17 +33,17 @@ def condense_prompt(prompt: ChatPromptValue) -> ChatPromptValue:
     llm_gpt3 = ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo-0125")
     messages = prompt.to_messages()
     num_tokens = llm_gpt3.get_num_tokens_from_messages(messages)
-    tool_messages = get_tool_messages(messages)
-    if tool_messages:
-        last_tool_message = tool_messages.pop()
-        new_last_tool_message = last_tool_message
+    last_message = messages.pop()
+    if isinstance(last_message, ToolMessage):
+        new_last_tool_message = last_message
         while num_tokens>4000:
-            new_last_tool_message = ToolMessage(content=last_tool_message.content[:1], additional_kwargs=last_tool_message.additional_kwargs, tool_call_id=last_tool_message.tool_call_id)
+            new_last_tool_message = ToolMessage(content=last_message.content[:1], additional_kwargs=last_message.additional_kwargs, tool_call_id=last_message.tool_call_id)
         #replace old tool message with new, truncated one
         for i, n in enumerate(messages):
-        if n == last_tool_message:
-            messages[i] = new_last_tool_message
+            if n == last_message:
+                messages[i] = new_last_tool_message
     return ChatPromptValue(messages=messages)
+
 
 def create_openai_tools_agent(
     llm: BaseLanguageModel, tools: Sequence[BaseTool], prompt: ChatPromptTemplate
