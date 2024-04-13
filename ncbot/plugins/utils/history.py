04de435d951d1c6b779d95ecc7c5cba83747b8db
KyTDK <71435411+KyTDK@users.noreply.github.com>
2024-04-14 03:50:44 +1000

    Update history.py

--- ncbot/plugins/utils/history.py
+++ ncbot/plugins/utils/history.py
@@ -85,7 +85,7 @@ class MemoryHistoryUtil():
                             entry['data']['content'] = content
                             print("tokens_in_history: " + str(tokens_in_history) + " index_value: " + str(tokens_in_history-self.TOKEN_LIMIT) + " Content_length: " + str(len(content)))
                         else:
-                            entry = memory_dict.pop(data'
+                            entry = memory_dict.pop('data')
                         tokens_in_history=llm_gpt3.get_num_tokens(content)+self.count_tokens_in_dict(memory_dict, llm_gpt3)
         memory_dict.insert(0, entry)
         return memory_dict
