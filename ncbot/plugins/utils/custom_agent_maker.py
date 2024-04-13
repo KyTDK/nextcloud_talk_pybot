d7ed4c2730d0107ed9362b0758f3a7f65631216a
Kyan Taber <taberkyan@gmail.com>
2024-04-13 15:51:49 +1000

    c

--- ncbot/plugins/utils/custom_agent_maker.py
+++ ncbot/plugins/utils/custom_agent_maker.py
@@ -19,8 +19,8 @@ def condense_prompt(prompt: ChatPromptValue) -> ChatPromptValue:
     last_message = messages.pop()
     while num_tokens > 4000:
         last_message.content = last_message.content[:-1]
+        last_message = BaseMessage(content=last_message.content[:-1], type=last_message.type)
         num_tokens = llm_gpt3.get_num_tokens_from_messages(messages)+llm_gpt3.get_num_tokens_from_messages([last_message])
-        print(num_tokens + " "+last_message)
     messages.append(last_message)
     return ChatPromptValue(messages=messages)
 
