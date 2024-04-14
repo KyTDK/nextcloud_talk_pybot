1b75956aa84afb88584ffadd659f2d3c7100adf8
KyTDK <71435411+KyTDK@users.noreply.github.com>
2024-04-14 17:21:55 +1000

    Update history.py

--- ncbot/plugins/utils/history.py
+++ ncbot/plugins/utils/history.py
@@ -84,6 +84,7 @@ class MemoryHistoryUtil():
                     tokens_in_history=llm_gpt3.get_num_tokens(content)+self.count_tokens_in_dict(memory_dict, llm_gpt3)
                 else:
                     entry = memory_dict.pop(0)
+                    print(entry)
             else:
                 break
         memory_dict.insert(0, entry)
