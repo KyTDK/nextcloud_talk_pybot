35a0e1db79cf8002f75a9276d4eb30b25e2744ed
Kyan Taber <taberkyan@gmail.com>
2024-04-16 07:32:46 +1000

    q

--- ncbot/plugins/utils/history.py
+++ ncbot/plugins/utils/history.py
@@ -27,20 +27,18 @@ class MemoryHistoryUtil():
     def clear_memory(self, conversation_token):
         pass
 
+    # def get_memory(self, conversation_token):
+    #     dict = self._get_from_memory(conversation_token)
+    #     if dict is None or not dict:  # Check for None and empty dictionary
+    #         return ConversationBufferMemory(return_messages=True, chat_memory=ChatMessageHistory(messages=[]))
+    #     memory_dict = self.__dict_to_message(dict)
+    #     history = ChatMessageHistory()
+    #     history.messages = history.messages + memory_dict
+    #     return ConversationBufferMemory(return_messages=True, chat_memory=history)
+
     def get_memory(self, conversation_token):
-        dict = self._get_from_memory(conversation_token)
-        if dict is None or not dict:  # Check for None and empty dictionary
-            return ConversationBufferMemory(return_messages=True, chat_memory=ChatMessageHistory(messages=[]))
-        memory_dict = self.__dict_to_message(dict)
-        history = ChatMessageHistory()
-        history.messages = history.messages + memory_dict
-        return ConversationBufferMemory(return_messages=True, chat_memory=history)
-
-    def get_base_memory(self, conversation_token):
-        dict = self._get_from_memory(conversation_token)
-        memory_dict = self.__dict_to_message(dict)
-        return dict
-
+        return self._get_from_memory(conversation_token)
+        
     # def save_memory(self, conversation_token, history: ConversationBufferMemory):
     #     chat_memory = history.chat_memory
     #     memory = self.__tuncate_memory(chat_memory)
