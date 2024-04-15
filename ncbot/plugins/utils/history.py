f99be72dfa396c4ccf4734a0d01a8abe6118a7cb
Kyan Taber <taberkyan@gmail.com>
2024-04-15 15:03:28 +1000

    x

--- ncbot/plugins/utils/history.py
+++ ncbot/plugins/utils/history.py
@@ -30,11 +30,11 @@ class MemoryHistoryUtil():
     def get_memory(self, conversation_token):
         dict = self._get_from_memory(conversation_token)
         if dict is None or not dict:  # Check for None and empty dictionary
-            return ConversationSummaryBufferMemory(return_messages=True, chat_memory=ChatMessageHistory(messages=[]))
+            return ConversationSummaryBufferMemory(chat_memory=ChatMessageHistory(messages=[]))
         memory_dict = self.__dict_to_message(dict)
         history = ChatMessageHistory()
         history.messages = history.messages + memory_dict
-        return ConversationSummaryBufferMemory(return_messages=True, chat_memory=history)
+        return ConversationSummaryBufferMemory(chat_memory=history)
 
     def get_base_memory(self, conversation_token):
         dict = self._get_from_memory(conversation_token)
