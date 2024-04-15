d2edd6bb898f4ea6cbcc87f442963946511ccc93
Kyan Taber <taberkyan@gmail.com>
2024-04-15 22:33:47 +1000

    d

--- ncbot/plugins/utils/history.py
+++ ncbot/plugins/utils/history.py
@@ -62,6 +62,7 @@ class MemoryHistoryUtil():
         # truncate token amount
         llm_gpt3 = ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo-0125")
         tokens_in_history = self.count_tokens_in_dict(memory_dict, llm_gpt3)
+        print("Before: "+str(memory_dict))
         entry = memory_dict.pop(0)
         while tokens_in_history > TOKEN_LIMIT:
             if memory_dict:
@@ -76,12 +77,12 @@ class MemoryHistoryUtil():
                     entry['data']['content'] = content
                     tokens_in_history = llm_gpt3.get_num_tokens(
                         content)+self.count_tokens_in_dict(memory_dict, llm_gpt3)
-                    print(str(tokens_in_history) + " "+str(entry))
                 else:
                     entry = memory_dict.pop(0)
             else:
                 break
         memory_dict.insert(0, entry)
+        print("After: "+str(memory_dict))
         return memory_dict
 
     def _get_index_key(self, conversation_token):
