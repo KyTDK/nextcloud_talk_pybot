65293fde4670003b9042247e5847826014361772
Kyan Taber <taberkyan@gmail.com>
2024-04-18 07:09:28 +1000

    c

--- ncbot/plugins/utils/history_redis.py
+++ ncbot/plugins/utils/history_redis.py
@@ -12,15 +12,15 @@ class RedisMemoryHistoryUtil(MemoryHistoryUtil):
     def __init__(self) -> None:
         super().__init__()
 
-    # def _save_to_memory(self, conversation_token, history):
-    #     self.clear_memory(conversation_token)
-    #     index_key = super()._get_index_key(conversation_token)
-    #     for ele in history:
-    #         conn.rpush(index_key, json.dumps(ele))
-
-    def _save_to_memory(self, conversation_token, summary):
+    def _save_to_memory(self, conversation_token, history):
+        self.clear_memory(conversation_token)
         index_key = super()._get_index_key(conversation_token)
-        conn.set(index_key, summary)
+        for ele in history:
+            conn.rpush(index_key, json.dumps(ele))
+
+    # def _save_to_memory(self, conversation_token, summary):
+    #     index_key = super()._get_index_key(conversation_token)
+    #     conn.set(index_key, summary)
 
     def _get_from_memory(self, conversation_token):
         if not super()._isStore():
@@ -30,11 +30,11 @@ class RedisMemoryHistoryUtil(MemoryHistoryUtil):
         dict = [json.loads(m.decode('utf-8')) for m in dict_range]
         return dict
     
-    def _get_from_memory(self, conversation_token):
-        if not super()._isStore():
-            return None
-        index_key = super()._get_index_key(conversation_token)
-        return conn.get(index_key)
+    # def _get_from_memory(self, conversation_token):
+    #     if not super()._isStore():
+    #         return None
+    #     index_key = super()._get_index_key(conversation_token)
+    #     return conn.get(index_key)
 
     def clear_memory(self, conversation_token):
         conn.delete(super()._get_index_key(conversation_token))
