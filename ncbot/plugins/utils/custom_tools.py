3f36bbbf9b7235d873f05af1baf859fc60906646
KyTDK <71435411+KyTDK@users.noreply.github.com>
2024-04-24 20:46:48 +1000

    Update custom_tools.py

--- ncbot/plugins/utils/custom_tools.py
+++ ncbot/plugins/utils/custom_tools.py
@@ -96,7 +96,7 @@ class ScrapeTool(BaseTool):
         texts = text_splitter.split_text(document.page_content)
         # Limit just to the first 3 chunks
         # so the code can be re-run quickly
-        first_few = texts[:3]
+        first_few = texts[:20]
         
         extractions = extractor.batch(
             [{"text": text} for text in first_few],
