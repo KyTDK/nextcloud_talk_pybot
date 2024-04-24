5392b447d1dd52bc3e2984a3eb74afcbd1f8f9db
KyTDK <71435411+KyTDK@users.noreply.github.com>
2024-04-24 15:02:51 +1000

    Update custom_tools.py

--- ncbot/plugins/utils/custom_tools.py
+++ ncbot/plugins/utils/custom_tools.py
@@ -4,6 +4,18 @@ from langchain_core.tools import BaseTool
 from langchain_community.document_loaders.chromium import AsyncChromiumLoader
 from langchain_community.document_transformers.html2text import Html2TextTransformer
 from langchain_community.utilities.searx_search import SearxSearchWrapper
+from langchain_community.vectorstores import FAISS
+from langchain_core.documents import Document
+from langchain_core.runnables import RunnableLambda
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+
+texts = text_splitter.split_text(document.page_content)
+vectorstore = FAISS.from_texts(texts, embedding=OpenAIEmbeddings())
+
+retriever = vectorstore.as_retriever(
+    search_kwargs={"k": 1}
+)  # Only extract from first document
 
 from langchain.callbacks.manager import (
     AsyncCallbackManagerForToolRun,
@@ -11,32 +23,42 @@ from langchain.callbacks.manager import (
 )
 
 class ScrapeInput(BaseModel):
-    urls: List[str] = Field(description="URL to scrape")
+    url: str = Field(description="URL to scrape")
+    query: str = Field(description="Type of content to extract, for example, key ideas, dates, names, etc")
 
 
 class ScrapeTool(BaseTool):
     name = "Scrape"
-    description = "Scrape and return text for a specific url. Always return your sources and the urls scraped"
+    description = "Extract specific text from a website for a specific URL. Always return your sources and the urls scraped"
     args_schema: Type[BaseModel] = ScrapeInput
     return_direct: bool = False
 
     def _run(
-        self, urls: List[str], run_manager: Optional[CallbackManagerForToolRun] = None
+        self, url: str, run_manager: Optional[CallbackManagerForToolRun] = None
     ) -> str:
         """Use the tool synchronously"""
         raise NotImplementedError("Calculator does not support async")
 
     async def _arun(
         self,
-        urls: List[str],
+        url: str,
+        query: str,
         run_manager: Optional[AsyncCallbackManagerForToolRun] = None,
     ) -> str:
         """Use the tool asynchronously."""
-        loader = AsyncChromiumLoader(urls)
-        docs = await loader.aload()
-        html2text = Html2TextTransformer()
-        docs_transformed = html2text.transform_documents(docs)
-        return docs_transformed[0].page_content
+        loader = AsyncChromiumLoader([url])
+        document = await loader.aload()[0]
+        texts = text_splitter.split_text(document.page_content)
+        vectorstore = FAISS.from_texts(texts, embedding=OpenAIEmbeddings())
+        
+        retriever = vectorstore.as_retriever(
+            search_kwargs={"k": 1}
+        )  # Only extract from first document
+        rag_extractor = {
+            "text": retriever | (lambda docs: docs[0].page_content)  # fetch content of top doc
+        } | extractor
+        results = rag_extractor.invoke(query)
+        return results
     
 class SearchInput(BaseModel):
     query: str = Field(description="What to lookup on the internet")
@@ -45,7 +67,7 @@ class SearchInput(BaseModel):
 
 class SearchTool(BaseTool):
     name = "Search"
-    description = "Browse the internet to return URLs and snippets of websites. Useful for when you need to answer questions about current events. You should ask targeted questions.  Always return your sources and urls of websites. Follow up with scrape to get additional information."
+    description = "Browse the internet to return URLs and snippets of websites. Useful for when you need to answer questions about current events. You should ask targeted questions. Always return your sources and urls of websites. Follow up with scrape to get additional information for complex questions."
     args_schema: Type[BaseModel] = SearchInput
     return_direct: bool = False
 
