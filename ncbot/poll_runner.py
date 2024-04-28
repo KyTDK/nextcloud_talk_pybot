93c32d255b2809e224fd55738dbe014237fe668c
Kyan Taber <taberkyan@gmail.com>
2024-04-28 21:35:57 +1000

    e

--- ncbot/poll_runner.py
+++ ncbot/poll_runner.py
@@ -42,12 +42,7 @@ def start():
                         #Start thread to handle message
                         thread = threading.Thread(target=run_async_task, args=(chatC,))
                         thread.daemon = True
-                        thread.start()
-                    else:
-                        #Queue message if its not a repeat
-                        if pending_chats[chatC.conversation_token] != chatC:
-                            unread_chats.append(chatC)
-                
+                        thread.start()                
         except Exception as e:
             traceback.print_exc()
             logger.error(e)
